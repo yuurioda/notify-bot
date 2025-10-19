6fa67d8e5a978ce1d604b287cca63e36709ab30b
yuurioda <yuurioda8@gmail.com>
2025-10-19 23:53:37 +0100

    Fix token

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Client } = require('discord.js-selfbot-v13');
  
 const cfg = {
-  token: 'process.env.TOKEN',            // Paste your account token here
-  logChannelId: 'process.env.LOG_CHANNEL_ID',     // Channel ID to send logs to
+  token: process.env.TOKEN,            
+  logChannelId: process.env.LOG_CHANNEL_ID,     
 };
  
 const client = new Client();
@@ -175,4 +175,5 @@ client.on('guildMemberAdd', async (member) => {
   }
 });
  
+
 client.login(cfg.token);
